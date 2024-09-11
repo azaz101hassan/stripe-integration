4127a63ec4f0659a8954f6719bd541f1891c7b0a
azaz101hassan <azaz101hassan@gmail.com>
2024-09-11 23:00:10 +0500

    Client - Checkout with stripe integration

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ShopPage from "./pages/ShopPage";
 import CheckoutPage from "./pages/CheckoutPage";
+import PaymentPage from "./pages/PaymentPage";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 import "./App.css";
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route exact path="/shop" element={<ShopPage />} />
         <Route exact path="/checkout" element={<CheckoutPage />} />
+        <Route exact path="/payment" element={<PaymentPage />} />
         <Route path="*" element={<Navigate to="/shop" replace />} />
       </Routes>
       <Footer />
--- client/src/components/PaymentSuccessful.js
+++ client/src/components/PaymentSuccessful.js
@@ -1,1 +1,25 @@
-// Create the PaymentSuccessful component here
+import React from "react";
+import { Card, CardContent, Typography, CardMedia } from "@mui/material";
+import complete from "../assets/complete.png";
+
+function PaymentSuccessful() {
+  return (
+    <div className="s-div">
+      <Card variant="outlined" className="s-payment" sx={{ maxWidth: 500 }}>
+        <CardMedia
+          component="img"
+          image={complete}
+          alt="Successful Payment"
+          className="s-media"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Payment Successful
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default PaymentSuccessful;
